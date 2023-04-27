5f9ec69b640e5710cb8465e342e9ead19adfdc67
bhattacharya-a-bt <bhattacharya.a.bt@gmail.com>
2023-04-27 10:28:49 -0700

    menet

--- R/multivariate_elasticnet.R
+++ R/multivariate_elasticnet.R
@@ -52,47 +52,158 @@ multivariate_elasticnet <- function(X,
     if (par){
         doParallel::registerDoParallel(n.cores)
         set.seed(seed)
-        models = glmnet::cv.glmnet(x = X,
+
+        models_0 = glmnet::cv.glmnet(x = X,
+                                     y = as.matrix(Y),
+                                     nfolds = nfolds,
+                                     parallel = par,
+                                     family = 'mgaussian',
+                                     keep = T,
+                                     alpha = 0,
+                                     intercept = T,
+                                     standardize = F,
+                                     trace.it = ifelse(verbose,1,0))
+
+        models_5 = glmnet::cv.glmnet(x = X,
+                                     y = as.matrix(Y),
+                                     nfolds = nfolds,
+                                     parallel = par,
+                                     family = 'mgaussian',
+                                     keep = T,
+                                     alpha = .5,
+                                     intercept = T,
+                                     standardize = F,
+                                     trace.it = ifelse(verbose,1,0))
+
+        models_1 = glmnet::cv.glmnet(x = X,
                                    y = as.matrix(Y),
                                    nfolds = nfolds,
-                                   alpha = alpha,
                                    parallel = par,
                                    family = 'mgaussian',
                                    keep = T,
+                                   alpha = 1,
+                                   intercept = T,
+                                   standardize = F,
                                    trace.it = ifelse(verbose,1,0))
     } else  {
         set.seed(seed)
-        models = glmnet::cv.glmnet(x = X,
+
+      models_0 = glmnet::cv.glmnet(x = X,
                                    y = as.matrix(Y),
                                    nfolds = nfolds,
-                                   alpha = alpha,
                                    parallel = par,
                                    family = 'mgaussian',
                                    keep = T,
+                                   alpha = 0,
+                                   intercept = T,
+                                   standardize = F,
+                                   trace.it = ifelse(verbose,1,0))
+
+      models_5 = glmnet::cv.glmnet(x = X,
+                                   y = as.matrix(Y),
+                                   nfolds = nfolds,
+                                   parallel = par,
+                                   family = 'mgaussian',
+                                   keep = T,
+                                   alpha = .5,
+                                   intercept = T,
+                                   standardize = F,
+                                   trace.it = ifelse(verbose,1,0))
+
+      models_1 = glmnet::cv.glmnet(x = X,
+                                   y = as.matrix(Y),
+                                   nfolds = nfolds,
+                                   parallel = par,
+                                   family = 'mgaussian',
+                                   keep = T,
+                                   alpha = 1,
+                                   intercept = T,
+                                   standardize = F,
                                    trace.it = ifelse(verbose,1,0))
     }
 
-    pred = models$fit.preval[,,which.min(models$cvm)]
-    r2.vec = unlist(sapply(1:ncol(Y),calc.r2,Y,pred)[1,])
+    pred_5 = models_5$fit.preval[,,which.min(models_5$cvm)]
+    pred_0 = models_0$fit.preval[,,which.min(models_0$cvm)]
+    pred_1 = models_1$fit.preval[,,which.min(models_1$cvm)]
+
+    r2.vec_0 = unlist(sapply(1:ncol(Y),calc.r2,Y,pred_0)[1,])
+    r2.vec_5 = unlist(sapply(1:ncol(Y),calc.r2,Y,pred_5)[1,])
+    r2.vec_1 = unlist(sapply(1:ncol(Y),calc.r2,Y,pred_1)[1,])
+
+    r2_mat = matrix(nrow = 3,
+                    ncol = ncol(Y))
+    colnames(r2_mat) = colnames(Y)
+    rownames(r2_mat) = c('alpha = 0','alpha = 0.5','alpha = 1')
+    r2_mat[1,] = r2.vec_0
+    r2_mat[2,] = r2.vec_5
+    r2_mat[3,] = r2.vec_1
+
     P = sapply(1:ncol(Y),function(x){
-      cor.test(Y[,x], pred[,x])$p.value
+      cor.test(Y[,x], pred_5[,x])$p.value
     })
+
+    r2_best = vector('numeric',length = ncol(Y))
+    best_alpha = vector('numeric',length = ncol(Y))
+    for (t in 1:length(r2_best)){
+      r2_best[t] = max(r2_mat[,t])
+      best_alpha[t] = rownames(r2_mat)[which.max(r2_mat[,t])]
+    }
 
     modelList = list()
     for (i in 1:ncol(Y)){
 
-        mod = tibble::tibble(SNP = colnames(X),
-                             Weight = coef(models,
-                                           s = 'lambda.min')[[i]][-1])
-        mod = subset(mod,Weight != 0)
-        best.pred = models$fit.preval[,i,which.min(models$cvm)]
-        reg = summary(lm(Y[,i]~best.pred))
-        modelList = rlist::list.append(modelList,
-                                       list(Transcript = colnames(Y)[i],
-                                            Model = mod,
-                                            R2 = r2.vec[i],
-                                            P = P[i],
-                                            Pred = pred[,i]))
+        if (best_alpha[i] == 'alpha = 0'){
+
+          mod = tibble::tibble(SNP = colnames(X),
+                               Weight = coef(models_0,
+                                             s = 'lambda.min')[[i]][-1])
+          mod = subset(mod,Weight != 0)
+          best.pred = models_0$fit.preval[,i,which.min(models$cvm)]
+          modelList = rlist::list.append(modelList,
+                                         list(Transcript = colnames(Y)[i],
+                                              Model = mod,
+                                              R2 = r2_best[i],
+                                              P = P[i],
+                                              Pred = pred_0[,i]))
+
+        }
+
+        if (best_alpha[i] == 'alpha = 0.5'){
+
+          ccc = coef(models_5,
+                     s = 'lambda.min')[[i]][-1]
+          mod = tibble::tibble(SNP = colnames(X),
+                               Weight = ccc)
+          mod = mod[cluster_weight(mod$Weight)$index,]
+          mod = subset(mod,Weight != 0)
+          best.pred = models_5$fit.preval[,i,which.min(models$cvm)]
+          modelList = rlist::list.append(modelList,
+                                         list(Transcript = colnames(Y)[i],
+                                              Model = mod,
+                                              R2 = r2_best[i],
+                                              P = P[i],
+                                              Pred = pred_0[,i]))
+
+        }
+
+        if (best_alpha[i] == 'alpha = 1'){
+
+          ccc = coef(models_1,
+                     s = 'lambda.min')[[i]][-1]
+          mod = tibble::tibble(SNP = colnames(X),
+                               Weight = ccc)
+          mod = mod[cluster_weight(mod$Weight)$index,]
+          mod = subset(mod,Weight != 0)
+          best.pred = models_1$fit.preval[,i,which.min(models$cvm)]
+          modelList = rlist::list.append(modelList,
+                                         list(Transcript = colnames(Y)[i],
+                                              Model = mod,
+                                              R2 = r2_best[i],
+                                              P = P[i],
+                                              Pred = pred_0[,i]))
+
+        }
+
 
     }
 
--- R/utils.R
+++ R/utils.R
@@ -1,57 +1,79 @@
-    "%^%" <- function(x, n) {
-        with(eigen(x), vectors %*% (values^n * t(vectors)))
-        }
-
-    .onUnload <- function (libpath) { library.dynam.unload("isoTWAS", libpath)}
-
-    calc.mse <- function(obs, pred){
-        if(is.vector(obs)) obs <- as.matrix(obs)
-        if(is.vector(pred)) pred <- as.matrix(pred)
-
-        n <- nrow(obs)
-        rss <- colSums((obs - pred)^2, na.rm = TRUE)
-        rss/n
+"%^%" <- function(x, n) {
+    with(eigen(x), vectors %*% (values^n * t(vectors)))
     }
 
-    calc.r2 <- function(i,obs,pred){
+.onUnload <- function (libpath) { library.dynam.unload("isoTWAS", libpath)}
 
-        r2 = summary(lm(obs[,i]~pred[,i]))$adj.r.sq
-        ct = cor.test(obs[,i],pred[,i])
-        if (is.na(r2)){r2 = 0}
-        p = ct$p.value
-        return(list(R2 = r2,
-                    P = p))
+calc.mse <- function(obs, pred){
+    if(is.vector(obs)) obs <- as.matrix(obs)
+    if(is.vector(pred)) pred <- as.matrix(pred)
 
-    }
+    n <- nrow(obs)
+    rss <- colSums((obs - pred)^2, na.rm = TRUE)
+    rss/n
+}
 
-    PRESS <- function(linear.model) {
-      #' calculate the predictive residuals
-      pr <- residuals(linear.model)/(1-lm.influence(linear.model)$hat)
-      #' calculate the PRESS
-      PRESS <- sum(pr^2)
+calc.r2 <- function(i,obs,pred){
 
-      return(PRESS)
-    }
+    r2 = summary(lm(obs[,i]~pred[,i]))$adj.r.sq
+    ct = cor.test(obs[,i],pred[,i])
+    if (is.na(r2)){r2 = 0}
+    p = ct$p.value
+    return(list(R2 = r2,
+                P = p))
 
-    pred_r_squared <- function(linear.model) {
-      #' Use anova() to get the sum of squares for the linear model
-      lm.anova <- anova(linear.model)
-      #' Calculate the total sum of squares
-      tss <- sum(lm.anova$'Sum Sq')
-      # Calculate the predictive R^2
-      pred.r.squared <- 1-PRESS(linear.model)/(tss)
+}
 
-      return(pred.r.squared)
-    }
+PRESS <- function(linear.model) {
+  #' calculate the predictive residuals
+  pr <- residuals(linear.model)/(1-lm.influence(linear.model)$hat)
+  #' calculate the PRESS
+  PRESS <- sum(pr^2)
+
+  return(PRESS)
+}
+
+pred_r_squared <- function(linear.model) {
+  #' Use anova() to get the sum of squares for the linear model
+  lm.anova <- anova(linear.model)
+  #' Calculate the total sum of squares
+  tss <- sum(lm.anova$'Sum Sq')
+  # Calculate the predictive R^2
+  pred.r.squared <- 1-PRESS(linear.model)/(tss)
+
+  return(pred.r.squared)
+}
 
 
-    get_best <- function(list_mods,G=G){
-        r2 = matrix(unlist(sapply(list_mods, function(y) sapply(y,function(x) x$R2))),
-                    nrow = G)
-        l_nrows = sapply(list_mods, function(y) sapply(y,function(x) nrow(x$Model)))
-        r2[l_nrows == 0] = 0
-        r2[is.na(r2)] = 0
-        bundle = cbind(apply(r2,1,which.max),1:G)
-        return(lapply(1:G,
-                      function(i) list_mods[[bundle[i,1]]][[bundle[i,2]]]))
-    }
+get_best <- function(list_mods,G=G){
+    r2 = matrix(unlist(sapply(list_mods, function(y) sapply(y,function(x) x$R2))),
+                nrow = G)
+    l_nrows = sapply(list_mods, function(y) sapply(y,function(x) nrow(x$Model)))
+    r2[l_nrows == 0] = 0
+    r2[is.na(r2)] = 0
+    bundle = cbind(apply(r2,1,which.max),1:G)
+    return(lapply(1:G,
+                  function(i) list_mods[[bundle[i,1]]][[bundle[i,2]]]))
+}
+
+cluster_weight = function(x){
+  if (length(x) == 0){return(list(sum = 0,
+                                  index = NA))}
+  if (length(x) == 1){return(list(sum = 0,
+                                  index = 1))}
+  p = p.adjust(pnorm(as.numeric(scale(x)),
+                     lower.tail=F),'fdr')
+  if (sum(p < 0.05) > 0){
+    return(list(sum = sum(p < 0.05),
+                index = which(p < 0.05)))
+  }
+  if (sum(p < .25) > 0){
+    return(list(sum = sum(p < 0.25),
+                index = which(p < 0.25)))
+  }
+  if (sum(p < .25) == 0){
+    return(list(sum = length(x),
+                index = 1:length(x)))
+  }
+}
+
